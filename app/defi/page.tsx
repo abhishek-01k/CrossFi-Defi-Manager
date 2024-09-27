4543d50ba393aef504535c97a57240cff2733f99
kamalbuilds <geniusamansingh@gmail.com>
2024-09-27 23:42:55 +0530

    defi pg 2

--- app/defi/page.tsx
+++ app/defi/page.tsx
@@ -2,9 +2,13 @@
 
 export default function Defi() {
   return (
-    <div>
+    <div style={{ width: '100vw', height: '100vh' }}>
       <h1>DeFi</h1>
-      <iframe src="https://warehouse.xfi.ms/public/dashboards/Elo6Dehl9yG8HPc6S0oZpRu50Y7xezEhviwgrElK?org_slug=default" width="full" height="full"></iframe>
+      <iframe 
+        src="https://warehouse.xfi.ms/public/dashboards/Elo6Dehl9yG8HPc6S0oZpRu50Y7xezEhviwgrElK?org_slug=default" 
+        style={{ width: '100%', height: '100%', border: 'none' }} 
+        title="DeFi Dashboard"
+      ></iframe>
     </div>
-  )
+  );
 }
